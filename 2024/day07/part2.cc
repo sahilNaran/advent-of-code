f718bc2011fece5b81f84729c7508cd14d05cb00
Sahil Naran <sahil.naran6@gmail.com>
2025-10-18 21:22:56 +0100

    Day7

--- 2024/day07/part2.cc
+++ 2024/day07/part2.cc
@@ -1,17 +1,74 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+long long merge(long long a, long long b) {
+  long long temp = b;
+  do {
+    a *= 10;
+    temp /= 10;
+  } while (temp);
+
+  return a + b;
+}
+
+bool dfs(long long target, vector<int> &nums, int index, int op,
+         long long total) {
+  int x = nums[index];
+  if (op == 0) {
+    total += x;
+  } else if (op == 1) {
+    total *= x;
+  } else {
+    total = merge(total, x);
+  }
+  if (index == nums.size() - 1) {
+    return target == total;
+  }
+  return dfs(target, nums, index + 1, 1, total) ||
+         dfs(target, nums, index + 1, 0, total) ||
+         dfs(target, nums, index + 1, -1, total);
+}
+
 int main() {
-    ifstream file("input.txt");
+  ifstream file("input.txt");
+  string line;
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
+  long long ans = 0;
+  while (getline(file, line)) {
+    long long x = 0;
+    bool colon = 0;
+    long long target = 0;
+    vector<int> nums;
+    for (int i = 0; i < line.size(); i++) {
+      if (line[i] == ':') {
+        colon = 1;
+        continue;
+      }
+      if (isdigit(line[i])) {
+        long long x = 0;
+        while (i < line.size() && isdigit(line[i])) {
+          x = 10 * x + (line[i] - '0');
+          i++;
+        }
+        i--;
+        if (!colon) {
+          target = x;
+        } else {
+          nums.push_back(x);
+        }
+      }
     }
+    if (dfs(target, nums, 0, 1, 0) || dfs(target, nums, 0, 0, 0) ||
+        dfs(target, nums, 0, -1, 0)) {
+      ans += target;
+    }
+  }
 
-    file.close();
-    return 0;
+  cout << ans;
+  file.close();
+  return 0;
 }
