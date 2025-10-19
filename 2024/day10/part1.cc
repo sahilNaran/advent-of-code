81420c6963f584bd6deae426fe84260a2c13489e
Sahil Naran <sahil.naran6@gmail.com>
2025-10-19 23:42:03 +0100

    day10 part1

--- 2024/day10/part1.cc
+++ 2024/day10/part1.cc
@@ -1,17 +1,74 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <set>
 #include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    ifstream file("input.txt");
+vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
+pair<int, int> operator+(const pair<int, int> a, const pair<int, int> b) {
+  return make_pair(a.first + b.first, a.second + b.second);
+}
+
+void search(vector<string> &grid, pair<int, int> me, int current,
+            set<pair<int, int>> &endpoints) {
+
+  if (current == 9) {
+    endpoints.insert(me);
+    return;
+  }
+
+  int H = grid.size();
+  int W = grid[0].size();
+
+  for (const auto &direction : directions) {
+    pair<int, int> newPos = me + direction;
+
+    if (newPos.first < 0 || newPos.first >= H || newPos.second < 0 ||
+        newPos.second >= W) {
+      continue;
     }
 
-    file.close();
-    return 0;
+    int nextLevel = current + 1;
+    if (grid[newPos.first][newPos.second] == (nextLevel + '0')) {
+      search(grid, newPos, nextLevel, endpoints);
+    }
+  }
+}
+
+int main() {
+  ifstream file("input.txt");
+
+  vector<string> grid;
+  string line;
+  while (getline(file, line)) {
+    grid.push_back(line);
+  }
+  file.close();
+
+  int H = grid.size();
+  int W = grid[0].size();
+
+  vector<pair<int, int>> locations;
+  for (size_t r = 0; r < H; r++) {
+    for (size_t c = 0; c < W; c++) {
+      if (grid[r][c] == '0') {
+        locations.push_back(make_pair(r, c));
+      }
+    }
+  }
+
+  int ans = 0;
+  for (auto &loc : locations) {
+    set<pair<int, int>> endpoints;
+    search(grid, loc, 0, endpoints);
+    ans += endpoints.size();
+  }
+
+  cout << ans;
+
+  return 0;
 }
--- 2024/day10/part2.cc
+++ 2024/day10/part2.cc
@@ -1,17 +1,77 @@
-#include <iostream>
 #include <fstream>
+#include <functional>
+#include <iostream>
+#include <numeric>
 #include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    ifstream file("input.txt");
+vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
-    }
+pair<int, int> operator+(const pair<int, int> a, const pair<int, int> b) {
+  return make_pair(a.first + b.first, a.second + b.second);
+}
 
-    file.close();
+int search(vector<string> &grid, pair<int, int> me, pair<int, int> direction,
+           int current) {
+
+  if (current == 9) {
+    return 1;
+  }
+
+  pair<int, int> newPos = me + direction;
+  int H = grid.size();
+  int W = grid[0].size();
+
+  if (newPos.first < 0 || newPos.first >= H || newPos.second < 0 ||
+      newPos.second >= W) {
     return 0;
+  }
+
+  int nextLevel = current + 1;
+  if (grid[newPos.first][newPos.second] != (nextLevel + '0')) {
+    return 0;
+  }
+
+  return transform_reduce(directions.begin(), directions.end(), 0, plus<>(),
+                          [&grid, &newPos, &nextLevel](const auto &direction) {
+                            return search(grid, newPos, direction, nextLevel);
+                          });
+}
+
+int main() {
+  ifstream file("sample.txt");
+
+  vector<string> grid;
+  string line;
+  while (getline(file, line)) {
+    grid.push_back(line);
+  }
+  file.close();
+
+  int H = grid.size();
+  int W = grid[0].size();
+
+  vector<pair<int, int>> locations;
+  for (size_t r = 0; r < H; r++) {
+    for (size_t c = 0; c < W; c++) {
+      if (grid[r][c] == '0') {
+        locations.push_back(make_pair(r, c));
+      }
+    }
+  }
+
+  int ans = 0;
+  for (auto &loc : locations) {
+    ans += transform_reduce(directions.begin(), directions.end(), 0, plus<>(),
+                            [&grid, &loc](const auto &direction) {
+                              return search(grid, loc, direction, 0);
+                            });
+  }
+
+  cout << ans;
+
+  return 0;
 }
