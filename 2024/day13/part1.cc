83c440478ee6c3403b121e771deadf0a4641c8bc
Sahil Naran <sahil.naran6@gmail.com>
2025-11-05 21:48:44 +0000

    day 13

--- 2024/day13/part1.cc
+++ 2024/day13/part1.cc
@@ -1,17 +1,62 @@
-#include <iostream>
+#include <cstdio>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    ifstream file("input.txt");
+  ifstream file("input.txt");
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
+  // sorry learning to type on this new keyboard :)
+
+  auto determinate = [](int &aX, int &aY, int &bX, int &bY) {
+    long long detA = aX * bY - bX * aY;
+    return detA;
+  };
+
+  string line;
+  int aX, aY, bX, bY, pX, pY = 0;
+
+  long long ans = 0;
+  while (getline(file, line)) {
+    if (line.empty()) {
+      long long detA = determinate(aX, aY, bX, bY);
+      if (detA == 0) {
+        aX = aY = bX = bY = pX = pY = 0;
+        continue;
+      }
+
+      long long detAa = determinate(pX, pY, bX, bY);
+      long long detAb = determinate(aX, aY, pX, pY);
+
+      if (detAa % detA != 0 || detAb % detA != 0) {
+        aX = aY = bX = bY = pX = pY = 0;
+        continue;
+      }
+
+      long long aPresses = detAa / detA;
+      long long bPresses = detAb / detA;
+
+      if (!(aPresses > 0 && bPresses > 0)) {
+        cout << aPresses;
+        aX = aY = bX = bY = pX = pY = 0;
+        continue;
+      }
+
+      ans += aPresses * 3 + bPresses;
+
+      aX = aY = bX = bY = pX = pY = 0;
+      continue;
     }
+    sscanf(line.c_str(), "Button A: X+%d, Y+%d", &aX, &aY);
+    sscanf(line.c_str(), "Button B: X+%d, Y+%d", &bX, &bY);
+    sscanf(line.c_str(), "Prize: X=%d, Y=%d", &pX, &pY);
+  }
 
-    file.close();
-    return 0;
+  file.close();
+
+  cout << ans;
+
+  return 0;
 }
--- 2024/day13/part2.cc
+++ 2024/day13/part2.cc
@@ -1,17 +1,62 @@
-#include <iostream>
+#include <cstdio>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
 int main() {
-    ifstream file("input.txt");
+  ifstream file("input.txt");
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
+  // sorry learning to type on this new keyboard :)
+
+  auto determinate = [](long long &aX, long long &aY, long long &bX,
+                        long long &bY) { return aX * bY - bX * aY; };
+
+  string line;
+  long long aX = 0, aY = 0, bX = 0, bY = 0;
+  long long pX = 0, pY = 0;
+  unsigned long long ans = 0;
+
+  auto processSet = [&]() {
+    if (aX == 0 && aY == 0 && bX == 0 && bY == 0) {
+      return;
     }
 
-    file.close();
-    return 0;
+    pX += 10000000000000ll;
+    pY += 10000000000000ll;
+    long long detA = determinate(aX, aY, bX, bY);
+
+    if (detA != 0) {
+      long long detAa = determinate(pX, pY, bX, bY);
+      long long detAb = determinate(aX, aY, pX, pY);
+      if (detAa % detA == 0 && detAb % detA == 0) {
+
+        long long aPresses = detAa / detA;
+        long long bPresses = detAb / detA;
+
+        if (aPresses >= 0 && bPresses >= 0) {
+          ans += aPresses * 3 + bPresses;
+        }
+      }
+    }
+    aX = aY = bX = bY = pX = pY = 0;
+  };
+
+  while (getline(file, line)) {
+    if (line.empty()) {
+      processSet();
+      continue;
+    }
+    sscanf(line.c_str(), "Button A: X+%lld, Y+%lld", &aX, &aY);
+    sscanf(line.c_str(), "Button B: X+%lld, Y+%lld", &bX, &bY);
+    sscanf(line.c_str(), "Prize: X=%lld, Y=%lld", &pX, &pY);
+  }
+  processSet();
+
+  file.close();
+
+  cout << ans;
+
+  return 0;
 }
