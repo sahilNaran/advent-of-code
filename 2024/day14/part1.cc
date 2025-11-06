c91e19adff1950ba1d9cd3c9ae1d8d2a62072a02
Sahil Naran <sahil.naran6@gmail.com>
2025-11-06 16:41:57 +0000

    day 14

--- 2024/day14/part1.cc
+++ 2024/day14/part1.cc
@@ -1,17 +1,30 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    ifstream file("input.txt");
+  ifstream file("input.txt");
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
-    }
+  string line;
+  int seconds = 100;
+  int H = 103;
+  int W = 101;
 
-    file.close();
-    return 0;
+  vector<vector<int>> quadrant(2, vector<int>(2, 0));
+  while (getline(file, line)) {
+    int px, py, vx, vy;
+    sscanf(line.c_str(), "p=%d,%d v=%d,%d", &px, &py, &vx, &vy);
+    int newX = ((px + vx * seconds) % W + W) % W;
+    int newY = ((py + vy * seconds) % H + H) % H;
+
+    if (newX != W / 2 && newY != H / 2)
+      quadrant[newX > W / 2][newY > H / 2]++;
+  }
+
+  cout << quadrant[0][0] * quadrant[0][1] * quadrant[1][0] * quadrant[1][1];
+  file.close();
+  return 0;
 }
--- 2024/day14/part2.cc
+++ 2024/day14/part2.cc
@@ -1,17 +1,95 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <limits>
+#include <set>
 #include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 int main() {
-    ifstream file("input.txt");
+  ifstream file("input.txt");
 
-    string line;
-    while (getline(file, line)) {
-        // Process input here
+  string line;
+  int H = 103;
+  int W = 101;
+
+  vector<pair<pair<int, int>, pair<int, int>>> robotsAndPositions;
+  while (getline(file, line)) {
+    int px, py, vx, vy;
+    sscanf(line.c_str(), "p=%d,%d v=%d,%d", &px, &py, &vx, &vy);
+
+    robotsAndPositions.push_back({{px, py}, {vx, vy}});
+  }
+
+  file.close();
+
+  auto print = [&](vector<pair<int, int>> robots, int time) {
+    cout << "Time elapsed: " << time << endl;
+
+    set<pair<int, int>> robotSet(robots.begin(), robots.end());
+
+    for (int c = 0; c < H; c++) {
+      for (int r = 0; r < W; r++) {
+        if (robotSet.count({r, c})) {
+          cout << "X";
+        } else {
+          cout << ".";
+        }
+      }
+      cout << endl;
+    }
+    cout << endl;
+  };
+
+  auto calculateVariance = [&](int seconds) {
+    vector<pair<int, int>> robots;
+
+    for (auto robot : robotsAndPositions) {
+
+      int newX =
+          ((robot.first.first + robot.second.first * seconds) % W + W) % W;
+      int newY =
+          ((robot.first.second + robot.second.second * seconds) % H + H) % H;
+      robots.push_back({newX, newY});
     }
 
-    file.close();
-    return 0;
+    double meanX = 0, meanY = 0;
+
+    for (auto robot : robots) {
+      meanX += robot.first;
+      meanY += robot.second;
+    }
+    meanX /= robots.size();
+    meanY /= robots.size();
+
+    double varianceX = 0, varianceY = 0;
+    for (auto robot : robots) {
+      double deltaX = robot.first - meanX;
+      double deltaY = robot.second - meanY;
+      varianceX += deltaX * deltaX;
+      varianceY += deltaY * deltaY;
+    }
+    varianceX /= robots.size();
+    varianceY /= robots.size();
+    return make_pair(varianceX + varianceY, robots);
+  };
+
+  int secondsElapsed = 0;
+  double minVariance = numeric_limits<double>::max();
+
+  int range = 10000;
+  for (int i = 0; i < range; i++) {
+    auto [variance, robots] = calculateVariance(i);
+    if (minVariance > variance) {
+      minVariance = variance;
+      secondsElapsed = i;
+      cout << "new minimum at time elapsed: " << i << " variance, " << variance
+           << endl;
+      print(robots, i);
+    }
+  }
+
+  return 0;
 }
